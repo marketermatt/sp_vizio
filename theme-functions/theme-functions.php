f1ed9447dbfc2dbedefd26d7ce42d7abcdef2ccf
gaganphp <mailtogagan990@gmail.com>
2015-05-26 17:20:51 +0530

    Added woocommerece support code
    Added woocommerece support code in theme-functions.php file

--- theme-functions/theme-functions.php
+++ theme-functions/theme-functions.php
@@ -480,3 +480,10 @@ if ( is_wp_error( $response ) ) {
   
 }
 }
+
+
+// woocmerce support
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
